6a97cd64af31e7662e2d67d2c86fcbf311b3feb9
Alazar <alazardemessie17@gmail.com>
2022-02-25 12:26:47 +0300

    DrawWindow() Controls has been improved and get responsive

--- ethKernel/ethKernel.c
+++ ethKernel/ethKernel.c
@@ -11,10 +11,10 @@ int main() {
     get_vesa_mode_info();
     SetupScreen();
 
-    int color = 0x00ff00;           // RGB
+    int color = 0x159c49;           // RGB
 
-    DrawWindow(0, 0, 0, 0, 5, "Title", 1);
-
+    DrawBackground(color);
+    DrawWindow(30, 30, 0, 0, 5, "Title", 1);
 
 
     printf(itoa(Rect->x), -1, -1, 0);
--- include/gui/window.c
+++ include/gui/window.c
@@ -7,7 +7,6 @@ struct RECT Controls[3];        // close, maximize, minimize
 struct BITMAP Bitmap;
 
 
-
 void InitWindow(int x, int y) {
     // default window values.
     // Setting up the RECT
@@ -24,23 +23,23 @@ void InitWindow(int x, int y) {
 
     // Controls
     // close
-    int width = 25;
+    int width = 25, ControlResizeResolution = 10, ControlResizeCoordinate = TitleBar.height / 5;
     Controls[0].x = Rect->x + Rect->width - width;
-    Controls[0].y = Rect->y;
-    Controls[0].width = width;
-    Controls[0].height = TitleBar.height;
+    Controls[0].y = Rect->y + ControlResizeCoordinate;
+    Controls[0].width = width - ControlResizeResolution;
+    Controls[0].height = TitleBar.height - ControlResizeResolution;
 
     // maximize
     Controls[1].x = Rect->x + Rect->width - width * 2;
-    Controls[1].y = Rect->y;
-    Controls[1].width = width;
-    Controls[1].height = TitleBar.height;
+    Controls[1].y = Rect->y + ControlResizeCoordinate;
+    Controls[1].width = width - ControlResizeResolution;
+    Controls[1].height = TitleBar.height - ControlResizeResolution;
 
     //minimize
     Controls[2].x = Rect->x + Rect->width - width * 3;
-    Controls[2].y = Rect->y;
-    Controls[2].width = width;
-    Controls[2].height = TitleBar.height;
+    Controls[2].y = Rect->y + ControlResizeCoordinate;
+    Controls[2].width = width - ControlResizeResolution;
+    Controls[2].height = TitleBar.height - ControlResizeResolution;
 
     // setting up the window
     Window.x = Rect->x;
@@ -59,25 +58,25 @@ void DrawWindow(int x, int y, int width, int height, int ShadowValue, uint8_t *t
     InitWindow(x, y);
 
     // Draw Window Shadow
-    DrawRectangle(Rect->x + ShadowValue, Rect->y + ShadowValue, Rect->height, Rect->width, 0x000000);
-    FillRect(Rect->x + ShadowValue, Rect->y + ShadowValue, Rect->width, Rect->height, 0x696863);
+    // DrawRectangle(Rect->x + ShadowValue, Rect->y + ShadowValue, Rect->height, Rect->width, 0x000000);
+    FillRect(Rect->x + ShadowValue, Rect->y + ShadowValue, Rect->width, Rect->height, 0x39463e);
 
     // Draw window
-    DrawRectangle(Rect->x, Rect->y, Rect->height, Rect->width, 0x000000);
+    // DrawRectangle(Rect->x, Rect->y, Rect->height, Rect->width, 0x000000);
     FillRect(Rect->x, Rect->y, Rect->width, Rect->height, 0xbda267);
 
     // Draw Title bar
-    DrawRectangle(Rect->x, Rect->y, TitleBar.height, Rect->width, 0x000000);
-    FillRect(TitleBar.x, TitleBar.y, TitleBar.width, TitleBar.height, 0xcdcdcb);
+    // DrawRectangle(Rect->x, Rect->y, TitleBar.height, Rect->width, 0x000000);
+    FillRect(TitleBar.x, TitleBar.y, TitleBar.width, TitleBar.height, 0x32503c);
 
     // // Draw Controls
-    DrawRectangle(Controls[0].x, Rect->y, Controls[0].height, Controls[0].width, 0x000000);
-    DrawRectangle(Controls[1].x, Rect->y, Controls[1].height, Controls[1].width, 0x000000);
-    DrawRectangle(Controls[2].x, Rect->y, Controls[2].height, Controls[2].width, 0x000000);
+    // DrawRectangle(Controls[0].x, Rect->y, Controls[0].height, Controls[0].width, 0x000000);
+    // DrawRectangle(Controls[1].x, Rect->y, Controls[1].height, Controls[1].width, 0x000000);
+    // DrawRectangle(Controls[2].x, Rect->y, Controls[2].height, Controls[2].width, 0x000000);
 
     // filling 
-    FillRect(Controls[0].x, Controls[0].y, Controls[0].width, Controls[0].height, 0x850f19);
-    FillRect(Controls[1].x, Controls[0].y, Controls[1].width, Controls[1].height, 0xffff00);
+    FillRect(Controls[0].x, Controls[0].y, Controls[0].width, Controls[0].height, 0xff0000);
+    FillRect(Controls[1].x, Controls[1].y, Controls[1].width, Controls[1].height, 0xffff00);
     FillRect(Controls[2].x, Controls[2].y, Controls[2].width, Controls[2].height, 0x00ff00);
 
 }
